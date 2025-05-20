77b57ea4d54dfa97921988c6c957f7a7ac7d8361
Emanuele <emanuele.ricca.10@gmail.com>
2025-05-20 12:54:05 +0200

    added cls_cluster_dict param for mapping class to cluster_id

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from training_systems.coop import CoOpSystem
 import torch
 import os
 from datetime import datetime
+
 if __name__ == "__main__":
-    #take the device name from DEVICE env variable
+    # take the device name from DEVICE env variable
     device = os.getenv("DEVICE", "cuda:0")
     print(f"Using device: {device}")
 
@@ -16,21 +17,45 @@ if __name__ == "__main__":
 
     print(f"Using {'CoOp' if use_coop else 'CoCoOp'} for training")
 
-    train_cls = CoOpSystem if use_coop else CoCoOpSystem
+    # dictionary to map class names to cluster IDs
+    # cls_cluster_dict = {
+    #     "class1": 0,
+    #     "class2": 1,
+    #     "class3": 2,
+    #     # Add more classes and their corresponding cluster IDs
+    # }
+    cls_cluster_dict = None
 
-    train_sys = train_cls(
-        batch_size=10,
-        device=device,
-        learning_rate=0.002,
-        weight_decay=0.0001,
-        momentum=0.9,
-        epochs=20,
-        run_name=f"run_{datetime.now().strftime('%Y%m%d_%H%M%S')}",
-        n_ctx=4,
-        ctx_init="",
-        class_token_position="end",
-        csc=False,
-        lambda_kl=0.7
-    )
-    
-    train_sys.train()
+    if use_coop:
+        train_sys = CoOpSystem(
+            batch_size=10,
+            device=device,
+            learning_rate=0.002,
+            weight_decay=0.0001,
+            momentum=0.9,
+            epochs=20,
+            run_name=f"run_{datetime.now().strftime('%Y%m%d_%H%M%S')}",
+            n_ctx=4,
+            ctx_init="",
+            class_token_position="end",
+            csc=False,
+            lambda_kl=0.7,
+        )
+        train_sys.train()
+    else:
+        train_sys = CoCoOpSystem(
+            batch_size=10,
+            device=device,
+            learning_rate=0.002,
+            weight_decay=0.0001,
+            momentum=0.9,
+            epochs=20,
+            run_name=f"run_{datetime.now().strftime('%Y%m%d_%H%M%S')}",
+            n_ctx=4,
+            ctx_init="",
+            class_token_position="end",
+            csc=False,
+            lambda_kl=0.7,
+            cls_cluster_dict=cls_cluster_dict,
+        )
+        train_sys.train()
--- training_systems/cocoop.py
+++ training_systems/cocoop.py
@@ -13,25 +13,30 @@ from torch.utils.tensorboard import SummaryWriter
 from torch.optim import SGD, Adam, AdamW
 from torch import nn
 from torch.optim.lr_scheduler import LambdaLR
+
+
 def harmonic_mean(a, b):
     """returns the harmonic mean of a and b"""
     return 2 * (a * b) / (a + b)
 
+
 class CoCoOpSystem:
-    def __init__(self,
-                 batch_size=16,
-                 device="cuda:0",
-                 learning_rate=0.002,
-                 weight_decay=0.0005,
-                 momentum=0.9,
-                 epochs=2,
-                 run_name="exp1",
-                 n_ctx=4,
-                 ctx_init="",
-                 class_token_position="end",
-                 csc=False,
-                 lambda_kl=0.5,
-                 ):
+    def __init__(
+        self,
+        batch_size=16,
+        device="cuda:0",
+        learning_rate=0.002,
+        weight_decay=0.0005,
+        momentum=0.9,
+        epochs=2,
+        run_name="exp1",
+        n_ctx=4,
+        ctx_init="",
+        class_token_position="end",
+        csc=False,
+        lambda_kl=0.5,
+        cls_cluster_dict=None,
+    ):
         self.batch_size = batch_size
         self.device = device
         self.learning_rate = learning_rate
@@ -54,23 +59,27 @@ class CoCoOpSystem:
         # Create a logger for the experiment
         self.writer = SummaryWriter(log_dir=f"runs/CoCoOp/{run_name}")
 
-        self.writer.add_hparams({
-            "batch_size": self.batch_size,
-            "learning_rate": self.learning_rate,
-            "weight_decay": self.weight_decay,
-            "momentum": self.momentum,
-            "epochs": self.epochs,
-            "n_ctx": self.n_ctx,
-            "ctx_init": self.ctx_init,
-            "class_token_position": self.class_token_position,
-            "csc": self.csc,
-            "max_epoch": self.max_epoch,
-            "lr_scheduler_type": self.lr_scheduler_type,
-            "warmup_epoch": self.warmup_epoch,
-            "warmup_type": self.warmup_type,
-            "lambda_kl": self.lambda_kl,
-            "warmup_cons_lr": self.warmup_cons_lr,
-        }, {})
+        self.writer.add_hparams(
+            {
+                "batch_size": self.batch_size,
+                "learning_rate": self.learning_rate,
+                "weight_decay": self.weight_decay,
+                "momentum": self.momentum,
+                "epochs": self.epochs,
+                "n_ctx": self.n_ctx,
+                "ctx_init": self.ctx_init,
+                "class_token_position": self.class_token_position,
+                "csc": self.csc,
+                "max_epoch": self.max_epoch,
+                "lr_scheduler_type": self.lr_scheduler_type,
+                "warmup_epoch": self.warmup_epoch,
+                "warmup_type": self.warmup_type,
+                "lambda_kl": self.lambda_kl,
+                "warmup_cons_lr": self.warmup_cons_lr,
+                "cls_cluster_dict": cls_cluster_dict,
+            },
+            {},
+        )
 
         # Get dataloaders
 
@@ -85,7 +94,7 @@ class CoCoOpSystem:
         self.val_base, self.val_novel = split_data(self.val_set, self.base_classes)
         self.test_base, self.test_novel = split_data(self.test_set, self.base_classes)
 
-        #self.classnames, _ = embed_dataset_classnames(dataset_name, preprocess=preprocess, model=clip_model)
+        # self.classnames, _ = embed_dataset_classnames(dataset_name, preprocess=preprocess, model=clip_model)
 
         resolution = self.clip_model.visual.input_resolution
 
@@ -95,10 +104,17 @@ class CoCoOpSystem:
         cfg.TRAINER.COCOOP = EasyDict()
         cfg.TRAINER.COCOOP.CTX_LOAD = "./bin/coop/exp1_ctx_only.pth"
         cfg.TRAINER.COCOOP.N_CTX = self.n_ctx  # Number of context tokens
-        cfg.TRAINER.COCOOP.CTX_INIT = self.ctx_init  # Leave empty for random initialization
+        cfg.TRAINER.COCOOP.CTX_INIT = (
+            self.ctx_init
+        )  # Leave empty for random initialization
         cfg.TRAINER.COCOOP.PREC = "fp16"  # Precision for meta network
         cfg.INPUT = EasyDict()
-        cfg.INPUT.SIZE = [resolution, resolution]  # Must match CLIP model's input resolution
+        cfg.INPUT.SIZE = [
+            resolution,
+            resolution,
+        ]  # Must match CLIP model's input resolution
+
+        cfg.cls_cluster_dict = cls_cluster_dict
 
         # Instantiate the network and move it to the chosen device (GPU)
         self.model = CustomCLIP(
@@ -113,9 +129,13 @@ class CoCoOpSystem:
                 param.requires_grad_(False)
 
         print(f"Total parameters: {sum(p.numel() for p in self.model.parameters()):,}")
-        print(f"Total trainable parameters: {sum(p.numel() for p in self.model.parameters() if p.requires_grad):,}")
+        print(
+            f"Total trainable parameters: {sum(p.numel() for p in self.model.parameters() if p.requires_grad):,}"
+        )
 
-        self.optimizer = self.get_optimizer(self.learning_rate, self.weight_decay, self.momentum)
+        self.optimizer = self.get_optimizer(
+            self.learning_rate, self.weight_decay, self.momentum
+        )
         self.cost_function = nn.CrossEntropyLoss()
 
     def train(self):
@@ -123,7 +143,14 @@ class CoCoOpSystem:
             if current_epoch < self.warmup_epoch:
                 return self.warmup_cons_lr / self.learning_rate
             else:
-                cosine_decay = 0.5 * (1 + math.cos(math.pi * (current_epoch - self.warmup_epoch) / (self.max_epoch - self.warmup_epoch)))
+                cosine_decay = 0.5 * (
+                    1
+                    + math.cos(
+                        math.pi
+                        * (current_epoch - self.warmup_epoch)
+                        / (self.max_epoch - self.warmup_epoch)
+                    )
+                )
                 return cosine_decay
 
         self.lr_scheduler = LambdaLR(self.optimizer, lr_lambda)
@@ -135,18 +162,25 @@ class CoCoOpSystem:
 
         best_novel_accuracy = 0.0
         patience_counter = 0
-        patience = 4# adjustable
+        patience = 4  # adjustable
         best_model_path = os.path.join("runs/CoCoOp", self.run_name, "best_model.pth")
         c = 0
-        pbar = tqdm(total=self.max_epoch, desc="OVERALL TRAINING", position=0, leave=True)
+        pbar = tqdm(
+            total=self.max_epoch, desc="OVERALL TRAINING", position=0, leave=True
+        )
         for e in range(self.max_epoch):
-            base_train_total_loss, base_train_ce_accuracy, base_ce_loss, base_kl_loss = training_step_v2(
+            (
+                base_train_total_loss,
+                base_train_ce_accuracy,
+                base_ce_loss,
+                base_kl_loss,
+            ) = training_step_v2(
                 model=self.model,
                 dataset=self.train_base,
                 optimizer=self.optimizer,
                 batch_size=self.batch_size,
                 device=self.device,
-                lambda_kl=self.lambda_kl
+                lambda_kl=self.lambda_kl,
             )
 
             if e % print_epoch_interval == 0:
@@ -167,14 +201,24 @@ class CoCoOpSystem:
                 )
 
                 self.log_values(e, base_val_loss, base_val_accuracy, "validation_base")
-                self.log_values(e, novel_val_loss, novel_val_accuracy, "validation_novel")
+                self.log_values(
+                    e, novel_val_loss, novel_val_accuracy, "validation_novel"
+                )
 
                 self.writer.add_scalar(f"train_base/ce_loss", base_ce_loss, e)
-                self.writer.add_scalar(f"train_base/ce_accuracy", base_train_ce_accuracy, e)
+                self.writer.add_scalar(
+                    f"train_base/ce_accuracy", base_train_ce_accuracy, e
+                )
                 self.writer.add_scalar(f"train_base/kl_loss", base_kl_loss, e)
-                self.writer.add_scalar(f"train_base/total_loss", base_train_total_loss, e)
+                self.writer.add_scalar(
+                    f"train_base/total_loss", base_train_total_loss, e
+                )
 
-                pbar.set_postfix(train_acc=base_train_ce_accuracy, val_acc=base_val_accuracy, train_total_loss=base_train_total_loss)
+                pbar.set_postfix(
+                    train_acc=base_train_ce_accuracy,
+                    val_acc=base_val_accuracy,
+                    train_total_loss=base_train_total_loss,
+                )
 
                 if novel_val_accuracy > best_novel_accuracy:
                     best_novel_accuracy = novel_val_accuracy
@@ -183,7 +227,9 @@ class CoCoOpSystem:
                 else:
                     patience_counter += 1
                     if patience_counter >= patience:
-                        print(f"Stopping early at epoch {e} due to no improvement in novel accuracy.")
+                        print(
+                            f"Stopping early at epoch {e} due to no improvement in novel accuracy."
+                        )
                         break
 
             pbar.update(1)
@@ -193,14 +239,18 @@ class CoCoOpSystem:
         print("After training:")
         self.model.load_state_dict(torch.load(best_model_path))  # Load best model
         base_acc, novel_acc = self.compute_evaluation(c)
-        self.writer.add_scalars('Final metrics', {
-            'Harmonic Mean': harmonic_mean(base_acc, novel_acc),
-        }, global_step=0)
+        self.writer.add_scalars(
+            "Final metrics",
+            {
+                "Harmonic Mean": harmonic_mean(base_acc, novel_acc),
+            },
+            global_step=0,
+        )
         self.writer.close()
         self.save_model()
 
     def save_model(self, path="./bin/cocoop"):
-        #create folder if not exist
+        # create folder if not exist
         if not os.path.exists(path):
             os.makedirs(path)
         # Save the model
@@ -208,30 +258,49 @@ class CoCoOpSystem:
 
     def load_model(self, path="./bin"):
         # Load the model
-        self.model.load_state_dict(torch.load(os.path.join(path, f"{self.run_name}.pth")))
+        self.model.load_state_dict(
+            torch.load(os.path.join(path, f"{self.run_name}.pth"))
+        )
         self.model.eval()
         print(f"Model loaded from {path}")
 
     def compute_evaluation(self, epoch_idx, base=False):
         base_model = self.model if not base else self.clip_model
-        base_accuracy = test_step(base_model, self.test_base, self.base_classes, self.batch_size, self.device, label="test", base=base)
-        novel_accuracy = test_step(base_model, self.test_novel, self.novel_classes, self.batch_size, self.device, label="test", base=base)
+        base_accuracy = test_step(
+            base_model,
+            self.test_base,
+            self.base_classes,
+            self.batch_size,
+            self.device,
+            label="test",
+            base=base,
+        )
+        novel_accuracy = test_step(
+            base_model,
+            self.test_novel,
+            self.novel_classes,
+            self.batch_size,
+            self.device,
+            label="test",
+            base=base,
+        )
         # Log to TensorBoard
-        self.log_value(epoch_idx,  base_accuracy, "base_classes")
-        self.log_value(epoch_idx,  novel_accuracy, "novel_classes")
+        self.log_value(epoch_idx, base_accuracy, "base_classes")
+        self.log_value(epoch_idx, novel_accuracy, "novel_classes")
 
         return base_accuracy, novel_accuracy
 
     def get_optimizer(self, lr, wd, momentum):
-        optimizer = SGD([
-            {
-                "params": self.model.parameters()
-            }
-        ], lr=lr, weight_decay=wd, momentum=momentum)
+        optimizer = SGD(
+            [{"params": self.model.parameters()}],
+            lr=lr,
+            weight_decay=wd,
+            momentum=momentum,
+        )
 
         return optimizer
 
-    def log_value(self, step,  accuracy, prefix):
+    def log_value(self, step, accuracy, prefix):
         self.writer.add_scalar(f"{prefix}/accuracy", accuracy, step)
 
     def log_values(self, step, loss, accuracy, prefix):
