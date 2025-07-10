1cf43589ec1304e0339a7f284ae9816f87fbcf67
BacobaxUnitn <francesco.bassignana@studenti.unitn.it>
2025-07-10 15:51:24 +0200

    fix

--- utils/kl.py
+++ utils/kl.py
@@ -20,7 +20,7 @@ def get_kl_loss(device, inputs_novel, model, targets_novel, tmp_dataset):
         Tensor: A scalar tensor representing the KL divergence loss.
     """
     targets_novel_tensor = torch.tensor(targets_novel).to(device) if isinstance(targets_novel, list) else targets_novel
-    categories_novel_tensor = [tmp_dataset.idx2cat[c] for c in list(set(targets_novel))]
+    categories_novel_tensor = [tmp_dataset.idx2cat[c.item()] for c in list(set(targets_novel_tensor))]
     # print(f"input novel shape: {inputs_novel.shape} novel base: {targets_novel_tensor.shape}")
     with torch.no_grad():
         image_features_clip = model.clip_model.encode_image(inputs_novel)
