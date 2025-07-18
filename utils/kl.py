ffe7358c4e156f767e0632ada9046a55a0c2db08
BacobaxUnitn <francesco.bassignana@studenti.unitn.it>
2025-07-18 11:47:46 +0200

    debug

--- utils/kl.py
+++ utils/kl.py
@@ -46,6 +46,12 @@ def get_kl_loss(device, inputs_novel, model, targets_novel, tmp_dataset):
         #         [logit.item() for column_idx, logit in enumerate(img_logits) if column_idx in [tmp_dataset.cat2idx[c] for c in categories_novel_tensor]])
         # student_logits = torch.tensor(student_logits_tmp).to(device)
         # print(f"student logits shape: {student_logits.shape}, clip logits shape: {clip_logits.shape}")
+        print("NaN in student_logits:", torch.isnan(student_logits).any().item())
+        print("NaN in clip_logits:", torch.isnan(clip_logits).any().item())
+        print("Inf in student_logits:", torch.isinf(student_logits).any().item())
+        print("Inf in clip_logits:", torch.isinf(clip_logits).any().item())
+        print("student_logits:", student_logits)
+        print("clip_logits:", clip_logits)
         kl_loss = torch.nn.functional.kl_div(
             torch.nn.functional.log_softmax(student_logits, dim=-1),
             torch.nn.functional.softmax(clip_logits, dim=-1),
